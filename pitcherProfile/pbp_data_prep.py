c8e4bfd2079352cb61c7a6e0125685194b74dbd5
GriffinJack <griffinjacksanford@gmail.com>
2025-07-08 21:19:46 -0700

    added more data prep funcs

--- pitcherProfile/pbp_data_prep.py
+++ pitcherProfile/pbp_data_prep.py
@@ -21,20 +21,26 @@ def get_pbp(first, last, start, end):
 #Filter data based on count
 def count_filter(pbp_data, counts):
     #Count filters, can change later if needed.
-    hitters_count = ['1-0', '2-0', '3-1', '3-0']
-    pitchers_count = ['1-2', '0-2']
-    even_count = ['0-0', '1-1']
-
+    count_filters = {
+    'Ahead' : ['1-0', '2-0', '3-1', '3-0'],
+    'Behind': ['1-2', '0-2'],
+    'Even' : ['0-0', '1-1']
+    }
     print("hello")
     if counts == 'All':
         return pbp_data
-    elif counts == 'Ahead':
-        return pbp_data[pbp_data['count'].isin(pitchers_count)]
-    elif counts == 'Behind':
-        return pbp_data[pbp_data['count'].isin(hitters_count)]
-    elif counts == 'Even':
-        return pbp_data[pbp_data['count'].isin(even_count)]
+    elif counts in count_filters:
+        return pbp_data[pbp_data['count'].isin(count_filters[counts])]
     else:
         return pbp_data[pbp_data['count'] == counts]
+
+
+def autopct_threshold(pct, threshold=2):
+    return f'{pct:.1f}%' if pct > threshold else ''
+
+def pitch_sums(pbp_data, stand = None):
+    if stand is  not None:
+        pitch_counts = pbp_data[pbp_data['stand'] == stand]
+    return pbp_data['pitch_name'].value_counts().to_dict()
 
 
